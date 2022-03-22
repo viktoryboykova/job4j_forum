c050b1367599834af1c70bbdbd4829f03253c3b6
viktoryboykova <viktoryboykova@list.ru>
2022-03-22 15:27:22 +0300

    add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -war target/job4j_forum.war --spring.config.location=heroku.properties
---
+++ target/classes/db/insert.sql
@@ -0,0 +1,2 @@
+insert into posts (name) values ('О чем этот форум?');
+insert into posts (name) values ('Правила форума.');

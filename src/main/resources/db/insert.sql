2da0ad6af491598bff680bf16da20ccd6bd36b5d
viktoryboykova <viktoryboykova@list.ru>
2022-03-22 17:38:27 +0300

    update sql

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,2 +1,5 @@
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
+
+insert into authorities (authority) values ('ROLE_USER');
+insert into authorities (authority) values ('ROLE_ADMIN');
